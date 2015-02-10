db5b9ff461acab47b7eca40fa3446849fab56521
culakm <culak.martin@gmail.com>
2015-02-10 23:17:32 +0100

    prvy zoznam hotovy a funkcny

--- app/src/main/java/com/hellbilling/kapitola12/MainActivity.java
+++ app/src/main/java/com/hellbilling/kapitola12/MainActivity.java
@@ -1,39 +1,34 @@
 package com.hellbilling.kapitola12;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ListActivity {
+    private TextView selection;
+    private static final String[] items={"lorem", "ipsum", "dolor",
+            "sit", "amet",
+            "consectetuer", "adipiscing", "elit", "morbi", "vel",
+            "ligula", "vitae", "arcu", "aliquet", "mollis",
+            "etiam", "vel", "erat", "placerat", "ante",
+            "porttitor", "sodales", "pellentesque", "augue", "purus"};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.activity_main);
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                items));
+        selection=(TextView)findViewById(R.id.selection);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onListItemClick(ListView parent, View v, int position,
+                                long id) {
+        selection.setText(items[position]);
     }
 }
